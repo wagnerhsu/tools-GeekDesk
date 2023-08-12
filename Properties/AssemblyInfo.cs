e121a7acd8e6c2abff3bf6884489c849b2fa7d37
Wagner Xu <wagnerhsu@hotmail.com>
2023-08-12 21:04:30 +0800

    feat: update csproj format

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。 
-[assembly: AssemblyTitle("GeekDesk")]
-[assembly: AssemblyDescription("小巧、美观的桌面快速启动工具")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BookerLiu")]
-[assembly: AssemblyProduct("GeekDesk")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // 将 ComVisible 设置为 false 会使此程序集中的类型
 //对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
 //请将此类型的 ComVisible 特性设置为 true。
@@ -37,17 +25,3 @@ using System.Windows;
                                               //(未在页面中找到资源时使用，
                                               //、应用程序或任何主题专用资源字典中找到时使用)
 )]
-
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
-//通过使用 "*"，如下所示:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.1.3")]
-[assembly: AssemblyFileVersion("2.5.1.3")]
